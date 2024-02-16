baf6cc4b914cd3e250b9bac0a869ea2846f76bdf
itrabbi <hassan.rabbi6321@gmail.com>
2024-02-16 21:33:11 +0600

    login & Register

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,27 +1,114 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const User = require("../models/userModel")
+const HttpError = require("../models/errorModel")
+
+
+
+
+
+
+
+
 // =================== REGISTER A NEW USER
 // POST : api/users/register
 // UNPROTECTED
-const registerUser = (req, res, next) => {
-    res.jons("Register User")
+const registerUser = async (req, res, next) => {
+    try {
+        const { name, email, password, password2 } = req.body;
+        if (!name || !email || !password) {
+            return next(new HttpError("Fill in all fields.", 422))
+        }
+
+        const newEmail = email.toLowerCase()
+
+        const emailExists = await User.findOne({ email: newEmail })
+        if (emailExists) {
+            return next(new HttpError("Email already exists.", 422))
+        }
+
+        if ((password.trim()).length < 6) {
+            return next(new HttpError("Password should be at least 6 characters.", 422))
+        }
+
+        if (password != password2) {
+            return next(new HttpError("Password do not match.", 422))
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(password, salt)
+        const newUser = await User.create({ name, email: newEmail, password: hashedPass })
+        res.status(201).json(`New User ${newUser.email} registered.`)
+    }
+    catch (error) {
+        return next(new HttpError("User registration failed", 422))
+    }
 }
+
+
+
+
+
+
+
+
+
 
 
 
 // =================== LOGIN A REGISTERED USER
 // POST : api/users/login
 // UNPROTECTED
-const loginUser = (req, res, next) => {
-    res.jons("Login User")
+const loginUser = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return next(new HttpError("Fill in all fields.", 422))
+        }
+        const newEmail = email.toLowerCase();
+        const user = await User.findOne({ email: newEmail })
+        if (!user) {
+            return next(new HttpError("Invalid credentials.", 422))
+        }
+        const comparePass = await bcrypt.compare(password, user.password)
+        if (!comparePass) {
+            return next(new HttpError("Invalid credentials.", 422))
+        }
+        const { _id: id, name } = user;
+        const token = jwt.sign({ id, name }, process.env.JWT_SCERET, { expiresIn: "1d" })
+
+        res.status(200).json({ token, id, name })
+    } catch (error) {
+        return next(new HttpError("Login failed. Please check your credentials.", 422))
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
 // =================== USER PROFILE
 // POST : api/users/:id
 // PROTECTED
-const getUser = (req, res, next) => {
-    res.jons("User Profile")
+const getUser = async (req, res, next) => {
+    res.json("User Profile")
 }
+
+
+
+
+
+
+
 
 
 
@@ -30,9 +117,19 @@ const getUser = (req, res, next) => {
 // =================== CHANGE USER AVATAR (profile  picture)
 // POST : api/users/change-avatar
 // PROTECTED
-const changeAvatar = (req, res, next) => {
-    res.jons("Change user avatar")
+const changeAvatar = async (req, res, next) => {
+    res.json("Change user avatar")
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,9 +138,17 @@ const changeAvatar = (req, res, next) => {
 // =================== EDIT USER DETAILS (from profile)
 // POST : api/users/edit-user
 // PROTECTED
-const editUser = (req, res, next) => {
-    res.jons("Edit user deatails")
+const editUser = async (req, res, next) => {
+    res.json("Edit user deatails")
 }
+
+
+
+
+
+
+
+
 
 
 
@@ -52,8 +157,17 @@ const editUser = (req, res, next) => {
 // =================== GET AUTHORS
 // POST : api/users/authors
 // UNPROTECTED
-const getAuthors = (req, res, next) => {
-    res.jons("Get all user/authors")
+const getAuthors = async (req, res, next) => {
+    res.json("Get all user/authors")
 }
 
-module.exports = { registerUser, loginUser, getUser, changeAvatar, editUser ,getAuthors}
+
+
+
+
+
+
+
+
+
+module.exports = { registerUser, loginUser, getUser, changeAvatar, editUser, getAuthors }
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 app.use(express.json({extended: true}));
@@ -13,6 +14,9 @@ app.use(cors({credentials: true, orgin: "http://localhost:3000"}));
 
 app.use('/api/users', userRoutes)
 app.use('/api/posts', postRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 connect(process.env.MONGO_URI).then(app.listen(5000, () => console.log(`Server running on port ${process.env.PORT}`))).catch(error => {console.log(error)})
 
